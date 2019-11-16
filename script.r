9b403aa731d30d6cc82ecf2e0068db937fe575f1
ihavenonickname <gabrielblanksm@gmail.com>
2019-11-15 22:26:01 -0300

    categorizar preço

--- script.r
+++ script.r
@@ -24,7 +24,7 @@ converter_de_memoria = function (coluna) {
     return (coluna)
 }
 
-converter_de_camera = function (coluna) {
+converter_de_camera = function (coluna, ignorar_sem) {
     coluna = sapply(coluna, function (x) {
         x = tolower(x)
 
@@ -32,7 +32,9 @@ converter_de_camera = function (coluna) {
 
         r = as.double(r[[1]][2])
 
-        r = if (is.na(r)) 0 else r
+        if (is.na(r) && ignorar_sem) {
+            r = 0
+        }
 
         return (r)
     })
@@ -102,19 +104,44 @@ converter_de_tela = function (coluna) {
     return (coluna)
 }
 
+converter_de_preco = function (coluna) {
+    coluna = sapply(coluna, function (x) {
+        x = tolower(x)
+
+        r = regmatches(x, gregexpr('\\d+(?:\\.\\d+)?', x))
+
+        r = as.double(r)
+
+        if (is.na(r) || r > 5000) {
+            return (NA)
+        }
+
+        return (r)
+    })
+
+    return (coluna)
+}
+
 carregar_dataframe = function () {
     dataset = read.csv(file='./phones.csv', header=TRUE, sep=',')
 
     df = data.frame(
         "storage"=converter_de_memoria(dataset$internal_memory),
         "memory"=converter_de_memoria(dataset$RAM),
-        "pcamera"=converter_de_camera(dataset$primary_camera),
-        "scamera"=converter_de_camera(dataset$secondary_camera),
+        "pcamera"=converter_de_camera(dataset$primary_camera, FALSE),
+        "scamera"=converter_de_camera(dataset$secondary_camera, TRUE),
         "display"=converter_de_tela(dataset$display_resolution),
-        "os"=converter_de_os(dataset$OS)
+        "os"=converter_de_os(dataset$OS),
+        "price"=converter_de_preco(dataset$approx_price_EUR)
     )
 
     df = na.omit(df)
+
+    df[["category"]] = as.factor(cut(
+        df[["price"]],
+        breaks=c(0, 80, 120, 180, 250, 350, 500, 1400),
+        labels=c("D", "C", "CB", "B", "BA", "A", "A+")
+    ))
 
     return (df)
 }
@@ -123,4 +150,29 @@ df = carregar_dataframe()
 
 gc()
 
-plot(df$scamera~df$pcamera, ylim=c(0, 50), xlim=c(0, 50))
+print("Total de celulares:")
+print(nrow(df))
+
+print("Correlação entre câmera primária e câmera secundária:")
+print(cor(df[["scamera"]], df[["pcamera"]]))
+
+print("Correlação entre disco e memória RAM:")
+print(cor(df[["storage"]], df[["memory"]]))
+
+print("Correlação entre tela e câmera primária:")
+print(cor(df[["display"]], df[["pcamera"]]))
+
+print("Correlação entre tela e preço:")
+print(cor(df[["display"]], df[["price"]]))
+
+print("Correlação entre disco e preço:")
+print(cor(df[["storage"]], df[["price"]]))
+
+print("Correlação entre câmera primária e preço:")
+print(cor(df[["pcamera"]], df[["price"]]))
+
+print("Correlação entre memória RAM e preço:")
+print(cor(df[["memory"]], df[["price"]]))
+
+print("Categorias de preço:")
+print(table(df[["category"]]))
