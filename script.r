5ffd79f3a9aeb6312099f28a046841a920d45fb2
ihavenonickname <gabrielblanksm@gmail.com>
2019-11-17 02:00:05 -0300

    branquear a normalização

--- script.r
+++ script.r
@@ -1,139 +1,86 @@
-converter_de_memoria = function (coluna) {
+obter_medida_numerica = function (coluna, sufixo) {
+    padrao = paste('(\\d+(?:\\.\\d+)?)\\s', sufixo, sep='')
+
+    coluna = regmatches(coluna, regexec(padrao, coluna))
+
     coluna = sapply(coluna, function (x) {
-        x = tolower(x)
-
-        r = regmatches(x, gregexpr('\\d+', x))
-
-        if (is.na(r)) {
-            return (NA)
-        }
-
-        r = strtoi(r)
-
-        if (grepl("gb", x)) {
-            return (r * 1024)
-        }
-
-        if (grepl("mb", x)) {
-            return (r)
-        }
-
-        return (NA)
+        return (if (identical(x, character(0))) NA else x[[2]])
     })
+
+    coluna = as.double(coluna)
+}
+
+converter_de_memoria = function (coluna) {
+    coluna = trimws(coluna)
+    coluna = tolower(coluna)
+
+    col_mb = obter_medida_numerica(coluna, 'mb')
+    col_gb = obter_medida_numerica(coluna, 'gb')
+
+    col_gb = as.double(col_gb) * 1024
+
+    coluna = rep(NA, length(coluna))
+    coluna[which(!is.na(col_mb))] = col_mb[which(!is.na(col_mb))]
+    coluna[which(!is.na(col_gb))] = col_gb[which(!is.na(col_gb))]
 
     return (coluna)
 }
 
-converter_de_camera = function (coluna, ignorar_sem) {
-    coluna = sapply(coluna, function (x) {
-        x = tolower(x)
+converter_de_camera = function (coluna, na_vira_0) {
+    coluna = tolower(coluna)
+    coluna = obter_medida_numerica(coluna, 'mp')
 
-        r = regmatches(x, regexec('(\\d+(?:\\.\\d+)?)\\smp', x))
-
-        r = as.double(r[[1]][2])
-
-        if (is.na(r) && ignorar_sem) {
-            r = 0
-        }
-
-        return (r)
-    })
+    if (na_vira_0) {
+        coluna[is.na(coluna)] = 0
+    }
 
     return (coluna)
 }
 
 converter_de_os = function (coluna) {
-    coluna = sapply(coluna, function (x) {
-        x = tolower(x)
+    coluna = tolower(coluna)
 
-        if (grepl("windows", x)) {
-            return ("windows")
-        }
+    nova_coluna = rep("other", length(coluna))
+    nova_coluna[grepl("ios", coluna)] = "ios"
+    nova_coluna[grepl("windows", coluna)] = "windows"
+    nova_coluna[grepl("android", coluna)] = "android"
+    nova_coluna[grepl("blackberry", coluna)] = "blackberry"
+    nova_coluna[grepl("symbian", coluna)] = "symbian"
+    nova_coluna = as.factor(nova_coluna)
 
-        if (grepl("android", x)) {
-            return ("android")
-        }
-
-        if (grepl("ios", x)) {
-            return ("ios")
-        }
-
-        if (grepl("blackberry", x)) {
-            return ("blackberry")
-        }
-
-        if (grepl("symbian", x)) {
-            return ("symbian")
-        }
-
-        if (grepl("firefox", x)) {
-            return ("firefox")
-        }
-
-        if (grepl("linux", x)) {
-            return ("linux")
-        }
-
-        if (grepl("tizen", x)) {
-            return ("tizen")
-        }
-
-        if (x != "") {
-            return ("other")
-        }
-
-        return (NA)
-    })
-
-    coluna = as.factor(coluna)
-
-    return (coluna)
+    return (nova_coluna)
 }
 
 converter_de_tela = function (coluna) {
-    coluna = sapply(coluna, function (x) {
-        x = tolower(x)
-
-        r = regmatches(x, regexec('(\\d+(?:\\.\\d+)?)\\sinches', x))
-
-        r = as.double(r[[1]][2])
-
-        return (r)
-    })
+    coluna = tolower(coluna)
+    coluna = obter_medida_numerica(coluna, 'inches')
 
     return (coluna)
 }
 
 converter_de_preco = function (coluna) {
-    coluna = sapply(coluna, function (x) {
-        x = tolower(x)
-
-        r = regmatches(x, gregexpr('\\d+(?:\\.\\d+)?', x))
-
-        r = as.double(r)
-
-        if (is.na(r) || r > 5000) {
-            return (NA)
-        }
-
-        return (r)
-    })
+    coluna = tolower(coluna)
+    coluna = regmatches(coluna, gregexpr('\\d+(?:\\.\\d+)?', coluna))
+    coluna = as.double(coluna)
+    coluna[coluna > 5000] = NA
 
     return (coluna)
 }
 
 carregar_dataframe = function () {
-    dataset = read.csv(file='./phones.csv', header=TRUE, sep=',')
+    df = read.csv(file='./phones.csv', header=TRUE, sep=',')
 
-    df = data.frame(
-        "storage"=converter_de_memoria(dataset$internal_memory),
-        "memory"=converter_de_memoria(dataset$RAM),
-        "pcamera"=converter_de_camera(dataset$primary_camera, FALSE),
-        "scamera"=converter_de_camera(dataset$secondary_camera, TRUE),
-        "display"=converter_de_tela(dataset$display_resolution),
-        "os"=converter_de_os(dataset$OS),
-        "price"=converter_de_preco(dataset$approx_price_EUR)
-    )
+    df[["storage"]] = converter_de_memoria(df[["internal_memory"]])
+    df[["ram"]] = converter_de_memoria(df[["RAM"]])
+    df[["pcamera"]] = converter_de_camera(df[["primary_camera"]], FALSE)
+    df[["scamera"]] = converter_de_camera(df[["secondary_camera"]], TRUE)
+    df[["display"]] = converter_de_tela(df[["display_resolution"]])
+    df[["os"]] = converter_de_os(df[["OS"]])
+    df[["price"]] = converter_de_preco(df[["approx_price_EUR"]])
+
+    df = df[c("storage", "ram", "pcamera", "scamera", "display", "os", "price")]
+
+    df[['ram']][df[['ram']] > 50000] = NA
 
     df = na.omit(df)
 
@@ -148,8 +95,6 @@ carregar_dataframe = function () {
 
 df = carregar_dataframe()
 
-gc()
-
 print("Total de celulares:")
 print(nrow(df))
 
@@ -157,7 +102,7 @@ print("Correlação entre câmera primária e câmera secundária:")
 print(cor(df[["scamera"]], df[["pcamera"]]))
 
 print("Correlação entre disco e memória RAM:")
-print(cor(df[["storage"]], df[["memory"]]))
+print(cor(df[["storage"]], df[["ram"]]))
 
 print("Correlação entre tela e câmera primária:")
 print(cor(df[["display"]], df[["pcamera"]]))
@@ -172,7 +117,10 @@ print("Correlação entre câmera primária e preço:")
 print(cor(df[["pcamera"]], df[["price"]]))
 
 print("Correlação entre memória RAM e preço:")
-print(cor(df[["memory"]], df[["price"]]))
+print(cor(df[["ram"]], df[["price"]]))
 
 print("Categorias de preço:")
-print(table(df[["category"]]))
+print(summary(df[["category"]]))
+
+print("Sistemas operacionais:")
+print(summary(df[["os"]]))
