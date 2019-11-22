7614d29bdf40d5ca2c778783324f80a48900ca67
ihavenonickname <gabrielblanksm@gmail.com>
2019-11-21 21:24:44 -0300

    melhor

--- script.r
+++ script.r
@@ -2,13 +2,14 @@ source('dependencias.r')
 source('leitura-dados.r')
 source('classificacao.r')
 
-print('Carregando dados...')
+print('Lendo dados...')
 df = carregar_dataframe('./phones.csv')
+df_num = Filter(is.numeric, df)
 
 print('Gerando matriz de correlação')
 corrplot::corrplot(cor(df_num), method='color')
 
 print('Aplicando knn...')
 cm_knn = obter_melhor_knn(df, kinicial=2, kpasso=5)
-
 print(cm_knn)
+print(cm_knn[['k']])
