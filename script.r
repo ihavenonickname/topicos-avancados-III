d8d7bdc824d687e64549f7f41c943f889b44d658
ihavenonickname <gabrielblanksm@gmail.com>
2019-12-05 03:13:36 -0300

    feitoria

--- script.r
+++ script.r
@@ -9,7 +9,25 @@ print("Gerando matriz de correlação")
 df_num = Filter(is.numeric, df)
 corrplot::corrplot(cor(df_num), method="color")
 
+# A rotina abaixo demora vários segundos para rodar!
 print("Aplicando knn...")
 cm_knn = obter_melhor_knn(df, kinicial=2, kpasso=5)
 print(cm_knn)
 print(cm_knn[["k"]])
+
+print("Aplicando regressão linear...")
+linear_model = lm(preco ~ ram + hd, data=df)
+linear_prediction = predict(linear_model, df)
+print("Média de erro com regressão linear")
+print(mean(abs(df[["preco"]] - linear_prediction)))
+
+print("Aplicando regressão não-linear...")
+nonlinear_model = nls(preco ~ a1*ram^2 + a2*hd^2 + b1*ram + b2*hd + c, data=df, start=list(a1=1, b1=1, a2=1, b2=1, c=0))
+nonlinear_prediction = predict(nonlinear_model, df)
+print("Média de erro com regressão não-linear")
+print(mean(abs(df[["preco"]] - nonlinear_prediction)))
+
+print("Plotando regressões...")
+plot(sort(df[["preco"]]), col="green")
+points(sort(linear_prediction), col="blue")
+points(sort(nonlinear_prediction), col="red")
